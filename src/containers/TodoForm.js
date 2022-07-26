101eb91a4b377cc877a47d83a9f643918b6ade1d
samuelwstout <samuelwstout@gmail.com>
2022-07-26 00:40:44 -0500

    Edit todo build

--- src/containers/TodoForm.js
+++ src/containers/TodoForm.js
@@ -4,10 +4,12 @@ import { useParams } from 'react-router-dom'
 const TodoForm = ({folder}) => {
 
 const [createInput, setCreateInput] = useState('')
+const [editId, setEditId] = useState(0)
+const [editInput, setEditInput] = useState('')
 
 const params = useParams()
 
-const handleSubmit = (e) => {
+const handleSubmitCreate = (e) => {
   e.preventDefault()
   fetch(`http://localhost:9292/folders/${params.id}/todos`, {
     method: 'POST',
@@ -23,23 +25,41 @@ const handleSubmit = (e) => {
   setCreateInput('')
 }
 
+const handleSubmitUpdate = (e) => {
+  e.preventDefault()
+  fetch(`http://localhost:9292/folders/${params.id}/todos/${editId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: editInput
+    }),
+  })
+  .then(r => r.json())
+  .then(data => data)
+  setEditInput('')
+}
+
   return (
     <div>
         <h4>Create Todo:</h4>
-          <form onSubmit={handleSubmit}>
-            <input type='text' onChange={e => setCreateInput(e.target.value)} />
+          <form onSubmit={handleSubmitCreate}>
+            <input type='text' value={createInput} onChange={e => setCreateInput(e.target.value)} />
             <input type='submit' />
           </form>
         <h4>Edit Todo:</h4>
-        <form>
-          <select>
+        <form onSubmit={handleSubmitUpdate}>
+          <select onChange={e => setEditId(e.target.value)}>
             <option>Select a todo</option>
             {
               folder.todos.map((f) => {
-               return <option key={f.id}>{f.name}</option>
+               return <option key={f.id} value={f.id}>{f.name}</option>
               })
             }
           </select>
+          <input type='text' value={editInput} onChange={e => setEditInput(e.target.value)} />
+          <input type='submit' />
         </form>
     </div>
   )
