d5d23ba3247c409cdbac7225332f1fd4d705a6f3
samuelwstout <samuelwstout@gmail.com>
2022-08-01 14:17:59 -0500

    Create new react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Folders from './pages/Folders'
-import Folder from './pages/Folder'
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  console.log('hey')
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/folders" element={<Folders />} />
-        <Route path="/folders/:id/todos" element={<Folder />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
